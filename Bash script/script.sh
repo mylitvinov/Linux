fe3a3ddd41d59c4a20353e6fa25ab1b99b45e488
mylitvinov <vinchester30@mail.ru>
2021-12-11 14:38:38 +0300

    3 bash info

--- Bash script/script.sh
+++ Bash script/script.sh
@@ -3,3 +3,14 @@ phrase="Hello to you!"
 echo $phrase
 
 # Use ./script.sh to run the script.
+
+first_greeting="Nice to meet you!"
+later_greeting="How are you?"
+greeting_occasion=1
+
+if [ $greeting_occasion -lt 1 ]
+then
+echo $first_greeting
+else
+echo $later_greeting
+fi
