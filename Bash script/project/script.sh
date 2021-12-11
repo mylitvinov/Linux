2688fee8dd38468dbcd2e574b635bd2798bbceda
mylitvinov <vinchester30@mail.ru>
2021-12-11 16:19:06 +0300

    bash project

---
+++ Bash script/project/script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "🔥🔥🔥Beginning build!! 🔥🔥🔥"
+firstline=$(head -n 1 source/changelog.md)
+read -a splitfirstline <<<$firstline
+version=${splitfirstline[1]}
+echo "You are building version" $version
+echo 'Do you want to continue? (enter "1" for yes, "0" for no)'
+read versioncontinue
+
+if [ $versioncontinue -eq 1 ]; then
+  echo "OK"
+  for filename in source/*; do
+    if [ "$filename" == "source/secretinfo.md" ]; then
+      echo "Not copying" $filename
+    else
+      echo "Copying" $filename
+      cp $filename build/.
+    fi
+  done
+
+  cd build/
+  echo "Build version $version contains:"
+  ls
+
+else
+  echo "Please come back when you are ready"
+fi
