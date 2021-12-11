3df62b06b59ffa5eba7869aa1196a137a6179a82
mylitvinov <vinchester30@mail.ru>
2021-12-11 14:51:13 +0300

    4 bash info

--- Bash script/script.sh
+++ Bash script/script.sh
@@ -4,6 +4,8 @@ echo $phrase
 
 # Use ./script.sh to run the script.
 
+###############################################################################
+
 first_greeting="Nice to meet you!"
 later_greeting="How are you?"
 greeting_occasion=1
@@ -14,3 +16,21 @@ echo $first_greeting
 else
 echo $later_greeting
 fi
+
+
+
+################################################################################
+
+first_greeting="Nice to meet you!"
+later_greeting="How are you?"
+greeting_occasion=0
+while [ $greeting_occasion -lt 3 ]
+do
+  if [ $greeting_occasion -lt 1 ]
+  then
+    echo $first_greeting
+  else
+    echo $later_greeting
+  fi
+  greeting_occasion=$((greeting_occasion + 1))
+done
