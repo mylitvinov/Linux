594d4a2e5c97a288d1d79e6e7a6da4ae3bb3b4ed
mylitvinov <vinchester30@mail.ru>
2022-01-31 20:02:34 +0300

    Linux_scripts

--- scriptFor.sh
+++ scriptFor.sh
@@ -11,3 +11,42 @@ done
 for ((i=1; i<10; i++)); do
     echo "i = $i"
 done    
+
+################################################
+for var1 in first second third fourth fifth
+do
+echo The  $var1 item
+done
+
+###########################################
+for var2 in first "the second" "the third" "I’ll do it"
+do
+echo "This is: $var2"
+done
+
+###########################################
+
+
+file="myfile"
+for var3 in $(cat $file)
+do
+echo " $var3"
+done
+
+#####################################################
+fileUser="/etc/passwd"
+IFS=$'\n'
+for var4 in $(cat $fileUser)
+do
+echo " $var4"
+done
+
+# Если этот скрипт запустить, он выведет именно то, что от него требуется, давая, 
+# в каждой итерации цикла, доступ к очередной строке, записанной в файл.
+
+##############################################################
+
+
+
+
+
